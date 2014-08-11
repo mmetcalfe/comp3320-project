7f47fd16f2cd338ac671b834204fd34f4aa7039a
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2014-08-11 23:09:11 +1000

    Add GLFW main loop

--- src/program.cpp
+++ src/program.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "debug.h"
+
+void errorCallback(int error, const char* description) {
+    std::cerr << "GLFW ERROR: " << description << std::endl;
+}
+
 int main(int argc, char** argv) {
-    std::cout << "COMP3320" << std::endl;
+    glfwInit();
+
+    glfwSetErrorCallback(errorCallback);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
+//    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr); // Fullscreen
+
+    glfwMakeContextCurrent(window);
+
+    checkForAndPrintGLError(__FILE__, __LINE__);
+
+    // Initialise GLEW:
+    glewExperimental = GL_TRUE;
+    glewInit();
+    checkForAndPrintGLError(__FILE__, __LINE__);
+
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClearColor(0.5, 0.5, 0.5, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        checkForAndPrintGLError(__FILE__, __LINE__);
+        /* Swap front and back buffers */
+//        glfwSwapInterval(1); // v-sync
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GL_TRUE);
+
+//        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    // Clean up GLFW:
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
 
---
+++ src/util/debug.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <iomanip>
+
+inline void printGLError(GLenum error) {
+    printf("%s", "GL Error: ");
+    switch (error) {
+        case GL_NO_ERROR: printf("%s\n", "GL_NO_ERROR"); break;
+        case GL_INVALID_ENUM: printf("%s\n", "GL_INVALID_ENUM"); break;
+        case GL_INVALID_VALUE: printf("%s\n", "GL_INVALID_VALUE"); break;
+        case GL_INVALID_OPERATION: printf("%s\n", "GL_INVALID_OPERATION"); break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION: printf("%s\n", "GL_INVALID_FRAMEBUFFER_OPERATION"); break;
+        case GL_OUT_OF_MEMORY: printf("%s\n", "GL_OUT_OF_MEMORY"); break;
+        default: printf("%s (%d)\n", "Unknown error", error); break;
+    }
+}
+
+inline void checkForAndPrintGLError(const std::string label) {
+    auto err = glGetError();
+    if (err) {
+        printf("[%s] ", label.c_str());
+        printGLError(err);
+    }
+}
+
+inline void checkForAndPrintGLError(int num) {
+    auto err = glGetError();
+    if (err) {
+        printf("[%d] ", num);
+        printGLError(err);
+    }
+}
+
+inline void checkForAndPrintGLError(const char* file, int line) {
+    auto err = glGetError();
+    if (err) {
+        printf("[%s, %d] ", file, line);
+        printGLError(err);
+    }
+}
+
+// GLM printing:
+inline std::ostream &operator<< (std::ostream &out, const glm::vec3 &vec) {
+    out << "[" << vec.x << ", " << vec.y << ", "<< vec.z << "]";
+    return out;
+}
+
+
+inline std::ostream &operator<< (std::ostream &out, const glm::vec4 &vec) {
+    out << "[" << vec.x << ", " << vec.y << ", " << vec.z << ", " << vec.w << "]";
+    return out;
+}
+
+inline std::ostream &operator<< (std::ostream &out, const glm::mat3 &mat) {
+    out << std::endl;
+    for (int row = 0; row < 3; row++) {
+        out << "    ";
+        out << "[";
+        for (int col = 0; col < 3; col++) {
+            out << std::setw(6) << std::setprecision(3) << mat[col][row];
+            if (col < 2)
+                out << ", ";
+        }
+        out << "]";
+        if (row < 2)
+            out << ",";
+        out << std::endl;
+    }
+
+    return out;
+}
+
+inline std::ostream &operator<< (std::ostream &out, const glm::mat4 &mat) {
+    out << std::endl;
+    for (int row = 0; row < 4; row++) {
+        out << "    ";
+        out << "[";
+        for (int col = 0; col < 4; col++) {
+            out << std::setw(6) << std::setprecision(3) << mat[col][row];
+            if (col < 3)
+                out << ", ";
+        }
+        out << "]";
+        if (row < 3)
+            out << ",";
+        out << std::endl;
+    }
+
+    return out;
+}
