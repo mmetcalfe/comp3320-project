5fb2a0cb01661612fa0f4fae4c43f02a30ba5ba1
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2014-09-02 20:31:49 +1000

    Add take_photo script

---
+++ doc/background/images/take_photo.py
@@ -0,0 +1,49 @@
+import cv2
+
+import os.path
+
+def next_file_name():
+    num = 1
+    while True:
+        file_name = 'image%03d.png' % num
+        if not os.path.exists(file_name):
+            return file_name
+        num += 1
+ 
+# Camera 0 is the integrated web cam on my netbook
+camera_port = 0
+ 
+#Number of frames to throw away while the camera adjusts to light levels
+ramp_frames = 30
+
+print("Initializing camera...")
+ 
+# Now we can initialize the camera capture object with the cv2.VideoCapture class.
+# All it needs is the index to a camera port.
+camera = cv2.VideoCapture(camera_port)
+ 
+# Captures a single image from the camera and returns it in PIL format
+def get_image():
+ # read is the easiest way to get a full image out of a VideoCapture object.
+ retval, im = camera.read()
+ return im
+ 
+print("Ramping camera...")
+
+# Ramp the camera - these frames will be discarded and are only used to allow v4l2
+# to adjust light levels, if necessary
+for i in xrange(ramp_frames):
+ temp = get_image()
+print("Taking image...")
+
+# Take the actual image we want to keep
+camera_capture = get_image()
+file = next_file_name()
+
+# A nice feature of the imwrite method is that it will automatically choose the
+# correct format based on the file extension you provide. Convenient!
+cv2.imwrite(file, camera_capture)
+ 
+# You'll want to release the camera, otherwise you won't be able to create a new
+# capture object until your script exits
+del(camera)
